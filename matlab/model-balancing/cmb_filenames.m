416d3153462b50dab1f0d1381c26f3fef96dbd13
Wolfram Liebermeister <wolfram.liebermeister@gmail.com>
2020-09-17 17:44:18 +0200

    Rerun example model - now called "double_branch_model"

--- matlab/model-balancing/cmb_filenames.m
+++ matlab/model-balancing/cmb_filenames.m
@@ -28,20 +28,20 @@ end
 
 filenames.model_name     = model_name;
 filenames.network_sbml   = network_sbml_file;
-filenames.run_dir        = [ out_DIR '/simulations/' run ];
-filenames.graphics_dir   = [ out_DIR '/simulations/' run '/ps-files/' ];
-filenames.data           = [ out_DIR '/simulations/' run '/data/' ];
-%filenames.kinetic_true   = [ out_DIR '/simulations/' run '/data/kinetic_true.tsv'];
-filenames.state_true     = [ out_DIR '/simulations/' run '/data/true'];
-filenames.kinetic_data   = [ out_DIR '/simulations/' run '/data/kinetic_data.tsv'];
-filenames.state_data     = [ out_DIR '/simulations/' run '/data/state_data.tsv'];
-filenames.parameters_out = [ out_DIR '/simulations/' run '/data/parameters.tsv'];
-filenames.states_out     = [ out_DIR '/simulations/' run '/data/'];
-filenames.model_out      = [ out_DIR '/simulations/' run '/data/model.tsv'];
-filenames.report         = [ out_DIR '/simulations/' run '/data/report.txt'];
-filenames.options_file   = [ out_DIR '/simulations/' run '/data/options'];
-filenames.options_tsv    = [ out_DIR '/simulations/' run '/data/options.tsv'];
-filenames.result_file    = [ out_DIR '/simulations/' run '/data/results'];
+filenames.run_dir        = [ out_DIR '/' run ];
+filenames.graphics_dir   = [ out_DIR '/' run '/ps-files/' ];
+filenames.data           = [ out_DIR '/' run '/data/' ];
+%filenames.kinetic_true   = [ out_DIR '/' run '/data/kinetic_true.tsv'];
+filenames.state_true     = [ out_DIR '/' run '/data/true'];
+filenames.kinetic_data   = [ out_DIR '/' run '/data/kinetic_data.tsv'];
+filenames.state_data     = [ out_DIR '/' run '/data/state_data.tsv'];
+filenames.parameters_out = [ out_DIR '/' run '/data/parameters.tsv'];
+filenames.states_out     = [ out_DIR '/' run '/data/'];
+filenames.model_out      = [ out_DIR '/' run '/data/model.tsv'];
+filenames.report         = [ out_DIR '/' run '/data/report.txt'];
+filenames.options_file   = [ out_DIR '/' run '/data/options'];
+filenames.options_tsv    = [ out_DIR '/' run '/data/options.tsv'];
+filenames.result_file    = [ out_DIR '/' run '/data/results'];
 filenames.kinetic_model    = 'kinetic_model';
 filenames.metabolic_states = 'metabolic_states';
 
