c125716f97d309575bd4c34edee3f7b70bf41d45
Elad Noor <eladnoor@protonmail.com>
2021-07-06 22:59:15 +0300

    removing python files after moving them to a new repo on gitlab

--- python/README.md
+++
@@ -1,4 +0,0 @@
-The `model_balancing` python package
-====================================
-
-The project 
--- python/setup.py
+++
@@ -1,5 +0,0 @@
-import versioneer
-from setuptools import setup
-
-# Most arguments are set in the `setup.cfg`.
-setup(version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass())
